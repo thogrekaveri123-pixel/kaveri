e8b155b298a64fb3cc0881c421f1adaf3f373dcd
thogrekaveri123-pixel <thogrekaveri123@gmail.com>
2025-11-15 12:09:55 +0530

    Add Animal and Dog classes with sound method

--- Inherit.java
+++ Inherit.java
@@ -1,1 +1,20 @@
+public class Animal {
+    public void sound() {
+        System.out.println("Animal eats food");
+    }
+}
+public class Dog extends Animal {
+    @Override
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Dog d = new Dog();
 
+        a.sound();
+        d.sound();
+    }
+}
