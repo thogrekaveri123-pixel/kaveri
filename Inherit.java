666421e87d8dbb556b4a8eae38a57dfd5530ae25
thogrekaveri123-pixel <thogrekaveri123@gmail.com>
2025-11-24 15:44:39 +0530

    Fix method calls for Dog and Animal classes

--- Inherit.java
+++ Inherit.java
@@ -3,18 +3,21 @@ public class Dog {
         System.out.println("Dog Barks");
     }
 }
+
 public class Animal extends Dog {
     @Override
     public void Dog() {
         System.out.println("Animal eats food");
     }
 }
+
 public class Main {
     public static void main(String[] args) {
         Animal a = new Animal();
         Dog g = new Dog();
 
-        a.Dog();
-        d.Dog();
+        a.Dog();  
+        g.Dog();  
     }
 }
+
