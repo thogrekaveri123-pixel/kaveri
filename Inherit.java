daa995ee469777661b04c17e2d034599f4bc8996
thogrekaveri123-pixel <thogrekaveri123@gmail.com>
2025-11-24 15:56:35 +0530

    Update Inherit.java

--- Inherit.java
+++ Inherit.java
@@ -11,7 +11,7 @@ public class Animal extends Dog {
     }
 }
 
-public class Main {
+public class Inherit{
     public static void main(String[] args) {
         Animal a = new Animal();
         Dog g = new Dog();
